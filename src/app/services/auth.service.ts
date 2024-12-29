35c7707012f653491b38bbfdc6ea8914ed5dda58
josepereza <josepereza66@gmail.com>
2024-12-29 11:02:36 +0100

    first commit3

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,9 @@
-import { Injectable, signal } from '@angular/core';
+import { Injectable, signal,  PLATFORM_ID, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { User } from '../models/user.model';
+import { isPlatformBrowser } from '@angular/common';
+
 
 @Injectable({
   providedIn: 'root'
@@ -9,18 +11,25 @@ import { User } from '../models/user.model';
 export class AuthService {
   private apiUrl = 'http://217.76.60.62:3000/auth';
   //private apiUrl = 'http://localhost:3000/auth';
+  private isBrowser: boolean;
+
 
   private tokenSubject = new BehaviorSubject<string | null>(localStorage.getItem('token'));
   currentUser = signal<User | null>(null);
 
-  constructor(private http: HttpClient) {}
+  constructor(@Inject(PLATFORM_ID) private platformId: object  , private http: HttpClient) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+
+  }
 
   login(email: string, password: string): Observable<any> {
     return this.http.post<{access_token: string}>(`${this.apiUrl}/login`, { email, password })
       .pipe(
         tap(response => {
             console.log('authService',response)
-          localStorage.setItem('token', response.access_token);
+            if (this.isBrowser) {
+              localStorage.setItem('token', response.access_token);
+            }
           this.tokenSubject.next(response.access_token);
           //this.currentUser.set(response.user);
         })
@@ -33,11 +42,19 @@ export class AuthService {
     this.currentUser.set(null);
   }
   setToken(token: string): void {
-    localStorage.setItem('token', token);
+    if (this.isBrowser) {
+          localStorage.setItem('token', token);
+
+    }
   }
 
   getToken(): string | null {
-    return localStorage.getItem('token');
+
+    if (this.isBrowser) {
+         return localStorage.getItem('token');
+
+    }else return null
+
   }
 
   getToken2(): string | null {
